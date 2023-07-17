a3b6fa743e0f570eab14f98d36a18a86a9419735
vinney cavallo <vcavallo@gmail.com>
2023-07-17 21:49:18 +0000

    Fix tuffle-config for goerli deploy

--- truffle-config.js
+++ truffle-config.js
@@ -2,14 +2,43 @@
 require('babel-register')
 require('babel-polyfill')
 
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const mnemonic = process.env.MNEMONIC;
+const es_key = process.env.ETHERSCAN_KEY;
+const infuraProjectId = "6b38131efce6467ca32aec6ebe619b68";
+
+const provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: mnemonic
+  },
+  providerOrUrl: `https://goerli.infura.io/v3/${infuraProjectId}`,
+  numberOfAddresses: 1,
+  addressIndex: 9,
+  shareNone: true,
+  derivationPath: "m/44'/60'/0'/0"
+})
+
 module.exports = {
+  plugins: [
+    'truffle-plugin-verify',
+  ],
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       gas: 6000000,
       network_id: "*" // Match any network id
-    }
+    },
+    goerli: {
+      provider: () => provider,
+      network_id: 5, // Goerli's network ID
+      gas: 5500000, // Adjust the gas limit according to your contracts
+    },
+  },
+  api_keys: {
+    etherscan: es_key,
   },
   compilers: {
     solc: {
